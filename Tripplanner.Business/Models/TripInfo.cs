49d69a66f21738da1ec42c282a47c5c63320c301
Long <long20p@gmail.com>
2019-05-13 22:12:39 +0200

    Init

---
+++ Tripplanner.Business/Models/TripInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tripplanner.Business.Models
+{
+    public class TripInfo
+    {
+        public string TripId { get; set; }
+        public string Destination { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
